bbad2850de39fc8aba2ea77f275c5e6c2ca966df
Thiago Araujo <thiago.araujotga@outlook.com>
2021-02-06 14:41:42 -0300

    adding initial consts and app.listen

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
-const express = require("express")
-const bodyParser = require ("body-parser")
+const express = require("express");
+const bodyParser = require ("body-parser");
+const controllerTarefas = require("./controllers/controller-tarefas");
+const controllerUsuarios = require("./controllers/controller-usuarios");
+const port = 8080;
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+
+app.listen(port,()=>{
+    console.log(`Url Configurada. Testar em localhost:${port}`)
+})
